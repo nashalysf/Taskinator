f64d768d5ab5463fe5bcb3cace51c90080cb4f4c
nashalysf <nashalysf@gmail.com>
2022-08-09 21:46:01 -0400

    refactor js code

--- assets/script.js
+++ assets/script.js
@@ -6,25 +6,35 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 /* When we use square brackets [ ] in a selector, we're trying to select an HTML element by one of its attributes. 
 In this case, we're selecting the <input> element that has a name attribute set to a value of "task-name" */
 
-let createTaskHandler = function(event){
+let taskFormHandler = function(event){
     event.preventDefault();
     let taskNameInput = document.querySelector("input[name='task-name']").value;
     let taskTypeInput = document.querySelector("select[name='task-type']").value;
-    // create list item
-    let taskItemEl= document.createElement("li");
-    taskItemEl.className="task-item";
-    // create a div to hold task info and add to list item
-    let taskInfoEl = document.createElement('div');
-    // give class name
-    taskInfoEl.className = "task-info";
-    // add html content to div
-    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    taskItemEl.appendChild(taskInfoEl);
-    // add entire list item to list
-    tasksToDoEl.appendChild(taskItemEl); 
+    //package info to object
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);
 };
+//
+//
+let createTaskEl= function(taskDataObj){
+// create list item
+let taskItemEl= document.createElement("li");
+taskItemEl.className="task-item";
+// create a div to hold task info and add to list item
+let taskInfoEl = document.createElement('div');
+// give class name
+taskInfoEl.className = "task-info";
+ // add html content to div
+ taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+ taskItemEl.appendChild(taskInfoEl);
+  // add entire list item to list
+  tasksToDoEl.appendChild(taskItemEl); 
+}
 //bc event happens at the form not the btn
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 
 // difference between .innerHTML && .textContent:
